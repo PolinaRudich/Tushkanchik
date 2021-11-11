05737afec0acc55f540bb44468b4e050dade7256
Polina <rudichrudichrudich@mail.ru>
2021-11-11 05:09:25 +0300

    added Income class

--- Tushkanchik/Income.cs
+++ Tushkanchik/Income.cs
@@ -2,11 +2,42 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Tushkanchik;
 using System.Threading.Tasks;
 
 namespace Tushkanchik
 {
     class Income
     {
+        private double _amount;
+        private double _percent;
+        private string _to;
+        private string _from;
+        private int _card;
+        private bool _fixed;
+        private Income _category;
+        public Income()
+        {
+            _amount = 0;
+            _percent = 0;
+            _to = "";
+            _from = "";
+            _card = 0;
+            _fixed = true;
+            _category = null;
+        }
+        public Income( double amount,double cashback, string to, string from, int card, Income category, Account account )
+        {
+            _amount = amount;
+            _percent = cashback;
+            _to = to;
+            _from = from;
+            _card = card;
+            _category = category;
+            account.AddBalance(amount);
+
+        }
+      
+
     }
 }
