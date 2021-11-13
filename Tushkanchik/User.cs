8e0c76ac4f9d984eb46acd9517869fc8544861db
Polina <rudichrudichrudich@mail.ru>
2021-11-13 03:03:25 +0300

    changed fields,methods in cash account class, added new class user

---
+++ Tushkanchik/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tushkanchik
+{
+     public class User
+    {
+        private int? _index;
+        private string _name;
+        public User()
+        {
+            _index = null;
+            _name = "";
+        }
+        public User(int idx, string name)
+        {
+            _index = idx;
+            _name = name;
+        }
+
+    }
+}
