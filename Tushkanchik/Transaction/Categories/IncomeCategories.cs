d23bfa3aab88caf5feab02d7599159fec9cfe630
Polina <rudichrudichrudich@mail.ru>
2021-11-18 01:47:27 +0300

    added incomeCategories and expensesCategories

---
+++ Tushkanchik/Transaction/Categories/IncomeCategories.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tushkanchik.Transaction.Categories
+{
+    public class IncomeCategories : Categories { }
+}
