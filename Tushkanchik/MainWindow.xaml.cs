5c3d28acc31d5d2145025d51c366986d035b297a
Polina <rudichrudichrudich@mail.ru>
2021-11-19 00:45:20 +0300

    added-tab-control

--- Tushkanchik/MainWindow.xaml.cs
+++ Tushkanchik/MainWindow.xaml.cs
@@ -19,24 +19,24 @@ using Newtonsoft.Json;
 using JsonSerializer = System.Text.Json.JsonSerializer;
 using System.Diagnostics;
 
+
 namespace Tushkanchik
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for MainPage.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-        private const string _UsersPath = "C:/Users/Asus/source/repos/Tushkanchik/json/users.txt";
-        public List<User> _users;
         public MainWindow()
         {
-
-            _users = GetUsersFromJSON();
             InitializeComponent();
+            _users = GetUsersFromJSON();
             FillUsersComboBox();
-
-
         }
+        private const string _UsersPath = "C:/Users/Asus/source/repos/Tushkanchik/json/users.txt";
+        public List<User> _users;
+        public string _userName;
+        
         public List<User> GetUsersFromJSON()
         {
             StreamReader reader = new StreamReader(_UsersPath);
@@ -73,7 +73,7 @@ namespace Tushkanchik
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             string name = @holderName.Text;
-            if (name.Length==0)
+            if (name.Length == 0)
             {
                 holderName.ToolTip = "";
             }
@@ -87,36 +87,36 @@ namespace Tushkanchik
                 }
                 else
                 {
-                   
-                   _users.Add(user);
 
-                    //foreach (User u in users)
-                    //{
-                    //    Trace.WriteLine("Name: " + u.GetName());
-                    //}
+                    _users.Add(user);
+
                     string converted = JsonConvert.SerializeObject(_users);
                     // парсинг в строку чтоб записать тест в файл 
                     //Trace.WriteLine(converted);
                     File.WriteAllText(_UsersPath, converted);
                     usersList.Items.Add(user._name);
-                    MainPage mainPage = new MainPage();
-                   this.Hide();
-                    mainPage.Show();
+                    main.IsSelected = true;
+                    _userName = holderName.Text;
+                    nameOfUser.Content = _userName;
+                    entertab.IsEnabled = false;
                 }
-                
+
             }
-            
+
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
 
-            MainPage mainPage = new MainPage();
-            this.Hide();
-            mainPage.Show();
-
-
-
+          
+            if(usersList.Text!="")
+            {
+                main.IsSelected = true;
+            }
+            _userName = usersList.Text;
+            nameOfUser.Content = _userName;
+            entertab.IsEnabled = false;
         }
     }
 }
+
