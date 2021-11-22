ec5581521f45fd82b4dda071563c808a87548352
Polina <rudichrudichrudich@mail.ru>
2021-11-22 03:29:51 +0300

    Added initial Storage class

--- Tushkanchik/Storage.cs
+++ Tushkanchik/Storage.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace Tushkanchik
@@ -13,7 +15,53 @@ namespace Tushkanchik
        //cохрание категорий
         
         private static Storage _storage;
-
+        public  string UsersPath = Directory.GetCurrentDirectory() + "/json/users.txt";
+        public string CardsPath = Directory.GetCurrentDirectory() + "/json/cards.txt";
+        public string DepositsPath = Directory.GetCurrentDirectory() + "/json/deposits.txt";
+        public List<User> GetUsersFromJSON()
+        {
+            if (!File.Exists(UsersPath))
+            {
+                FileStream fs = File.Create(UsersPath);
+                fs.Close();
+                return new List<User>();
+            }
+            List<User> users = new List<User>();
+            string json = File.ReadAllText(UsersPath);
+            if (string.IsNullOrWhiteSpace(json))
+            {
+                users = new List<User>();
+            }
+            if (users is null)
+            {
+                users = new List<User>();
+            }
+            else
+            {
+                users = JsonSerializer.Deserialize<List<User>>(json);
+            }
+            return users;
+        }
+        public List<Card> GetCardsFromJSON()
+        {
+            if (!File.Exists(CardsPath))
+            {
+                FileStream fs = File.Create(CardsPath);
+                fs.Close();
+                return new List<Card>();
+            }
+            List<Card> cards = new List<Card>();
+            string json = File.ReadAllText(CardsPath);
+            if (string.IsNullOrWhiteSpace(json))
+            {
+                cards = new List<Card>();
+            }
+            else
+            {
+                cards = JsonSerializer.Deserialize<List<Card>>(json);
+            }
+            return cards;
+        }
         private Storage() { }
 
         public static Storage GetInstance()
@@ -24,5 +72,6 @@ namespace Tushkanchik
             }
             return _storage;
         }
+        public 
     }
 }
