2a6614551de0858c90137f696182dcee4e4785f8
Dmitry Ivanov <mr.dimas7778@bk.ru>
2021-11-27 23:48:59 +0300

    реализовал создание категорий

---
+++ Tushkanchik/Transactions/Categories/ExpenseCategorylogic.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows;
+using Tushkanchik.Transaction.Categories;
+
+namespace Tushkanchik.Transactions.Categories
+{
+    class ExpenseCategorylogic
+    {
+        private Storage _storage = Storage.GetInstance();
+        public void Create(string name)
+        {
+            ExpenseCategory category = new ExpenseCategory { Name = name };
+            List<ExpenseCategory> categories = _storage.ExpenseCategoryFromJSON();
+            if (categories.Contains(category))
+            {
+                MessageBox.Show("Такая категория уже существует");
+                return;
+            }
+            categories.Add(category);
+
+            string converted = JsonSerializer.Serialize(categories);
+            File.WriteAllText(_storage.ExpenseCategoryPath, converted);
+        }
+    }
+}
---
+++ Tushkanchik/Transactions/Categories/IncomeCategoryLogic.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows;
+using Tushkanchik.Transaction.Categories;
+
+namespace Tushkanchik.Transactions.Categories
+{
+    class IncomeCategoryLogic
+    {
+        private Storage _storage = Storage.GetInstance();
+        public void CreateCategory(string name)
+        {
+            IncomeCategory category = new IncomeCategory { Name = name };
+            var categories = _storage.IncomeCategoryFromJSON();
+            if (categories.Contains(category))
+            {
+                MessageBox.Show("Такая категория уже существует");
+                return;
+            }
+
+
+            categories.Add(category);
+
+            string converted = JsonSerializer.Serialize(categories);
+            File.WriteAllText(_storage.IncomeCategoryPath, converted);
+        }
+    }
+}
